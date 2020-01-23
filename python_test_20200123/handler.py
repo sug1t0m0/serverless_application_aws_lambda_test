09626355f847fb017f52d7a96e31746d89078fb5
tomo_s <f15c017d@gmail.com>
2020-01-23 21:08:28 +0900

    カスタムリクエストテンプレートを試してみる

--- python_test_20200123/handler.py
+++ python_test_20200123/handler.py
@@ -1,24 +1,31 @@
+# coding: utf-8
 import json
+import logging
+
+# ログの設定
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def hello(event, context):
+    """
+    AWS Lambda Handler関数
+    @Param event イベントデータ　APIGatewayからのデータ
+    @Param content Lambdaのランタイムデータ
+    @return APIGatewayのレスポンスデータ
+    """
+    # イベントデータの表示
+    logger.info('headers:' + str(event['headers']))
+    logger.info('body:' + str(event['body']))
+    # レスポンスbodyを作成
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
-
+    # レスポンスデータの作成
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
